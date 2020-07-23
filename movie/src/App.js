36d70a4f91b8ee66b82cc0c41865cdb3b8ed5cfc
parkjaehoon <yukihoon12@naver.com>
2020-07-23 23:17:40 +0900

    2-4강

--- movie/src/App.js
+++ movie/src/App.js
@@ -5,25 +5,36 @@ function Food({name}){
   return<h1> I Like f {name}</h1>
 }
 const foodILike = [{
+  id:1,
   name : 'kimbap'
 },
 {
+  id:2,
   name : 'chicken'
 },
 {
+  id:3,
   name : 'rice'
 },
 {
+  id:4,
   name : 'meat'
 },
 {
-  name : 'vagetable'
+  id:5,
+  name : 'vagetable',
 }]; 
+
+function renderFood(dish){
+  return<Food name={dish.name}/>
+}
 
 function App() {
   return (
-    <div><h1>hello</h1>
-      {foodILike.map(dish=><Food name={dish.name}></Food>)}
+    <div><h1>hello</h1> 
+      {foodILike.map(dish=>{
+        return<Food key = {dish.id} name={dish.name}/>
+    })} 
     </div>  
   )
 }
