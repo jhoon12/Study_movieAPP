50bb53d056f41276ac8918b9f9773435163af566
parkjaehoon <yukihoon12@naver.com>
2020-07-23 22:47:37 +0900

    map을 활용한 props

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
 
-function Food(props){
-  console.log(props);
-  return<h1> I Like f</h1>
+function Food({name}){
+  return<h1> I Like f {name}</h1>
 }
+const foodILike = [{
+  name : 'kimbap'
+},
+{
+  name : 'chicken'
+},
+{
+  name : 'rice'
+},
+{
+  name : 'meat'
+},
+{
+  name : 'vagetable'
+}]; 
 
 function App() {
   return (
     <div><h1>hello</h1>
-        <Food fav="Kimchi"/> 
-        <Food fav="1"/> 
-        <Food fav="2"/> 
-        <Food fav="3"/> 
+      {foodILike.map(dish=><Food name={dish.name}></Food>)}
     </div>  
   )
 }
