6a0a520c1b62c08ff087f12a6fdea13322077f73
parkjaehoon <yukihoon12@naver.com>
2020-07-24 09:19:34 +0900

    3.0

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,53 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-function Food({name, rating}){
-  return<><h1> I Like f {name}</h1>
-  <h4>{rating}/5.0 </h4>
-  </>
-
-}
-Food.propTypes = {
-  name:PropTypes.string.isRequired,
-}
-const foodILike = [{
-  id:1,
-  name : 'kimbap',
-  rating: 1.0
-},
-{
-  id:2,
-  name : 'chicken'
-  ,
-  rating: 2.0
-},
-{
-  id:3,
-  name : 'rice'
-  ,
-  rating: 1.0
-},
-{
-  id:4,
-  name : 'meat'
-  ,
-  rating: 3.0
-},
-{
-  id:5,
-  name : 'vagetable',
-  rating: 4.0
-}]; 
-
-
-function App() {
-  return (
-    <div><h1>hello</h1> 
-      {foodILike.map(dish=>{
-        return<Food key = {dish.id} name={dish.name} rating={dish.rating}/>
-    })}
-    </div>  
-  )
+class App extends React.Component{
+  state = {
+    count : 0,
+  }
+  add = ()=>{
+    return count +1;
+  }
+  minus = ()=>{
+    return count-1;
+  }
+  render(){
+    let a = 3;
+    return( 
+    <>
+    <h1>i am class component {a}</h1>
+    <button onCLick={this.add}>
+      Add
+    </button>
+    <button onClick={this.minus}>
+      minus
+    </button>
+    </>
+    )
+  }
 }
 
 export default App;
