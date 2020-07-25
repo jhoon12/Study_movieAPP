6143aeade8c1bcd24b25a4c4f814b3614c66cff5
parkjaehoon <yukihoon12@naver.com>
2020-07-25 23:10:28 +0900

    3-2

--- movie/src/App.js
+++ movie/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello1");
+  }
   state = {
     count : 0,
   }
@@ -11,7 +15,18 @@ class App extends React.Component{
   minus = ()=>{
     this.setState({count : this.state.count-1})
   }
+  componentDidMount(){
+    console.log("component renderd")
+  }//렌더링끝나고
+  componentDidUpdate(){
+    console.log("I just updateds");
+  }//이벤트 동작시(state가 바뀌니까 호출됨)
+
+  componentWillUnmount(){
+    console.log("bye");
+  }//다른페이지로 이동할 때
   render(){//setState를 사용시 렌더시 호출             
+    console.log("I am rendering");
     return( 
     <>
     <h1>i am class component {this.state.count}</h1>
@@ -21,6 +36,7 @@ class App extends React.Component{
     <button onClick={this.minus}>
       minus
     </button>
+
     </>
     )
   }
