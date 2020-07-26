f1bdc573a9d56c9ff0e3081034de67a7e6626a15
parkjaehoon <yukihoon12@naver.com>
2020-07-26 23:43:49 +0900

    0726

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,45 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello1");
-  }
+class App extends React.Component {
   state = {
-    count : 0,
+    isLoading: true,
+    movie:[]
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 4000);
   }
-  add = ()=>{
-    this.setState(current => ({count : current.count+1}))
-  }
-  minus = ()=>{
-    this.setState({count : this.state.count-1})
-  }
-  componentDidMount(){
-    console.log("component renderd")
-  }//렌더링끝나고
-  componentDidUpdate(){
-    console.log("I just updateds");
-  }//이벤트 동작시(state가 바뀌니까 호출됨)
-
-  componentWillUnmount(){
-    console.log("bye");
-  }//다른페이지로 이동할 때
-  render(){//setState를 사용시 렌더시 호출             
-    console.log("I am rendering");
-    return( 
-    <>
-    <h1>i am class component {this.state.count}</h1>
-    <button onClick={this.add}>
-      Add
-    </button>
-    <button onClick={this.minus}>
-      minus
-    </button>
-
-    </>
-    )
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
-
 export default App;
