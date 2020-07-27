c9f7fa2e89f0a0b9c77c0222315b193c0dc86fd9
parkjaehoon <yukihoon12@naver.com>
2020-07-27 09:17:16 +0900

    4-1
    axios

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
@@ -6,10 +7,11 @@ class App extends React.Component {
     isLoading: true,
     movie:[]
   };
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 4000);
+  getMovies = async () = {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  async componentDidMount() {
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
