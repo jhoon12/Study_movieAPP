9128bdf3f80fcc2ed1b7836344fc78c7f36b6505
parkjaehoon <yukihoon12@naver.com>
2020-07-27 20:21:59 +0900

    4-4

--- movie/src/App.js
+++ movie/src/App.js
@@ -25,12 +25,12 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading
-          ? <div class="loade">
-              <span class="loader_text">Loading...</span>
+          ? <div className="loade">
+              <span className="loader_text">Loading...</span>
             </div>
-          :<div class="movies">
+          :<div className="movies">
             {movies.map((movie) => {
               return( 
               <Movie
@@ -40,6 +40,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
                 ></Movie>
               );
             })}
--- movie/src/movie.js
+++ movie/src/movie.js
@@ -1,14 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres}) {
   return (
-    <div class="movies_move">
+    <div className="movies_move">
+        <label  htmlFor/>
       <img src={poster} alt={title} title={"영화 이미지"}></img>
-      <div class="movie_date">
-        <h3 class="movie_title">{title}</h3>
-        <h3 class="movie_year">{year}</h3>
-        <p class="movie_summary">{summary}</p>
+      <div className="movie_date">
+        <h3 className="movie_title">{title}</h3>
+        <h3 className="movie_year">{year}</h3>
+        <ul className = "genres"></ul>
+        {genres.map((genre,index) =>{
+            return (<li className="genres_genres" key={index}>{genre}</li>);
+        })}
+        <p className="movie_summary">{summary}</p> 
       </div>
     </div>
   );
@@ -20,6 +25,7 @@ Movie.prototype = {
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
+  genres:PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
